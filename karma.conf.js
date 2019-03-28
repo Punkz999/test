ded68d1ad11c22c0c09617a896d23b40d74329a5
punkz <punkz@gmail.com>
2019-03-28 13:37:02 +0530

    karma branch commit

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 //testchaneg
+//karama brahcn change welcome
 module.exports = function (config) {
   config.set({
     basePath: '',
